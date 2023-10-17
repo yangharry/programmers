206414c8660e36e2b143ede78404076f4a2bf6cb
yangharry <120225915+yangharry@users.noreply.github.com>
2023-10-17 11:31:11 +0900

    [level 1] Title: 삼총사, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -1,16 +1,12 @@
 function solution(number) {
-    let result = 0;
-
-    const combination = (current, start) => {
-        if (current.length === 3) {
-            result += current.reduce((acc, cur) => acc + cur, 0) === 0 ? 1 : 0;
-            return;
-        }
-
-        for (let i = start; i < number.length; i++) {
-            combination([...current, number[i]], i + 1);
+    var answer = 0;
+    for(let i =0; i < number.length-2; i++){
+        for(let j = i+1; j < number.length-1; j++){
+            for(let k = j+1; k < number.length; k++){
+                  const sum = number[i] + number[j] + number[k]
+                if(sum === 0) answer++
+            }
         }
     }
-    combination([], 0);
-    return result;
+    return answer;
 }
