38f4ff4ce08ef46a9155c8bd949bfaabcbb42f92
yangharry <120225915+yangharry@users.noreply.github.com>
2023-10-18 11:09:34 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -1,21 +1,25 @@
 function solution(X, Y) {
-    let result = '';
-    const numObj = {};
+    const commonNumbers = [...new Set(X.split(''))].filter((number) => {
+        return Y.includes(number);
+    }).sort((a, b) => b - a)
 
-    for (const char of X) {
-        numObj[char] = (numObj[char] || 0) + 1;
-    }
+    if (!commonNumbers.length) return '-1';
 
-    for (const char of Y) {
-        if (!numObj[char]) continue;
-        result += char;
-        numObj[char]--;
-    }
+    if (!Number(commonNumbers[0])) return '0';
 
-    if (result === '') return '-1';
-    if (+result === 0) return '0';
-    return [...result]
-        .map((num) => +num)
-        .sort((a, b) => b - a)
-        .join('');    
+    return commonNumbers.map((number) => {
+        const Xcount = X.split('').reduce((count, Xnumber) => {
+            if (Xnumber === number) return count += 1;
+
+            return count;
+        }, 0)
+
+        const Ycount = Y.split('').reduce((count, Ynumber) => {
+            if (Ynumber === number) return count += 1;
+
+            return count;
+        }, 0)        
+
+        return Xcount <= Ycount ? number.repeat(Xcount) : number.repeat(Ycount)
+    }).join('')
 }
