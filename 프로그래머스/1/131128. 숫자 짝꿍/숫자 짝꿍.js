5b0362ed6d2aec9c2013ca31b52f599425867a9e
yangharry <120225915+yangharry@users.noreply.github.com>
2023-10-18 11:20:35 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.30 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -1,24 +1,23 @@
-function solution(X, Y) {
-    var answer = '';
-    let commonNum = []
-    let objectX = X.split('').reduce((acc,cur)=>{
-        return (acc[cur] = (acc[cur] || 0) + 1), acc
-    },{})
-    let objectY = Y.split('').reduce((acc,cur)=>{
-        return (acc[cur] = (acc[cur] || 0) + 1), acc
-    },{})
+function solution(x, y){
+  let answer = "";
+  const xHash = new Map();
+  const yHash = new Map();
+
+  x.split("")
+      .forEach(i=> xHash.set(i, xHash.has(i) ?  xHash.get(i)  +1 : 1));
+  y.split("")
+      .forEach(i=> yHash.set(i, yHash.has(i) ? yHash.get(i) +1 : 1));
+
 
     for(let i = 9; i >= 0; i --){
-      const index = i.toString();
-      if(!objectX[index]) continue;
-      if(!objectY[index]) continue;
+        const index = i.toString();
+        if(!xHash.has(index)) continue;
+        if(!yHash.has(index)) continue;
 
-      const num = Math.min(objectX[index], objectY[index])
-      answer += index.repeat(num)
+        const num = Math.min(xHash.get(index), yHash.get(index))
+        answer += index.repeat(num)
     }
     if(answer.length === 0) return "-1"
     if(answer[0] === "0" ) return "0";
     return answer;
-
-   
 }
