01e86c0cc23c9aa6b697ecd23c0e5e5e6164f475
yangharry <120225915+yangharry@users.noreply.github.com>
2023-10-18 11:08:03 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -1,31 +1,21 @@
 function solution(X, Y) {
-    var answer = '';
-    let commonNum = []
-    let objectX = X.split('').reduce((acc,cur)=>{
-        return (acc[cur] = (acc[cur] || 0) + 1), acc
-    },{})
-    let objectY = Y.split('').reduce((acc,cur)=>{
-        return (acc[cur] = (acc[cur] || 0) + 1), acc
-    },{})
-    
-    let objectXkeys = Object.keys(objectX)
-    let objectYkeys = Object.keys(objectY)
-    
-    objectXkeys.forEach((e)=>{
-        if(objectYkeys.indexOf(e) != -1){
-            let length = objectX[e] < objectY[e] ? objectX[e] : objectY[e]  
-            for(let i = 0; i < length; i++){
-                commonNum.push(e)                
-            }
-        }
-    })
-    commonNum.sort((a,b)=>(a < b ? 1 : -1))
-    if(commonNum.length == 0){
-        answer = "-1"
-    } else if(Number(commonNum.join(''))==0){
-        answer = "0"
-    } else {
-        answer = commonNum.join('')
+    let result = '';
+    const numObj = {};
+
+    for (const char of X) {
+        numObj[char] = (numObj[char] || 0) + 1;
     }
-    return answer;
+
+    for (const char of Y) {
+        if (!numObj[char]) continue;
+        result += char;
+        numObj[char]--;
+    }
+
+    if (result === '') return '-1';
+    if (+result === 0) return '0';
+    return [...result]
+        .map((num) => +num)
+        .sort((a, b) => b - a)
+        .join('');    
 }
