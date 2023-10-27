37397421fad45eade02b74bcdc50ba335cb1152a
yangharry <120225915+yangharry@users.noreply.github.com>
2023-10-27 11:35:07 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 136 ms, Memory: 10836 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.js
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.js
@@ -4,40 +4,17 @@ const [nk] = (require('fs').readFileSync(process.platform === 'linux' ? '/dev/st
 
 const [n, k] = nk.split(' ').map((e) => (e = +e));
 
-const wheel = {};
-
-for (let i = 0; i < n; i++) {
-  let next = (i + 1) % n;
-  let prev = (i + n - 1) % n;
-  wheel[i] = { value: i + 1, prev: prev, next: next };
+const wheel = [];
+for (let i = 1; i <= n; i++) {
+  wheel.push(i);
 }
 
-let beforeLocation = 0;
-let location = 0;
-
-function next() {
-  return wheel[location].next;
-}
-
+let i = 0;
 let answer = [];
-for (let j = 0; j < n; j++) {
-  if (j != 0) {
-    beforeLocation = location;
-    for (let i = 0; i < k; i++) {
-      if (i == 1) {
-        wheel[wheel[beforeLocation].prev].next = wheel[beforeLocation].next;
-        wheel[wheel[beforeLocation].next].prev = wheel[beforeLocation].prev;
-        delete wheel[beforeLocation];
-      }
-      location = next();
-    }
-    answer.push(wheel[location].value);
-  } else {
-    for (let i = 1; i < k; i++) {
-      location = next();
-    }
-    answer.push(wheel[location].value);
-  }
+while (wheel.length > 0) {
+  i += k - 1;
+  i %= wheel.length;
+  answer.push(...wheel.splice(i, 1));
 }
 
 console.log(`<${answer.join(', ')}>`);
