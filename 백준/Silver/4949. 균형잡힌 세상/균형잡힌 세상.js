7134ecf2ee8e6491b4c85fefec6f4940304d5543
yangharry <120225915+yangharry@users.noreply.github.com>
2023-10-29 22:03:33 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 192 ms, Memory: 12344 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.js
@@ -0,0 +1,32 @@
+const [...input] = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'ex0.txt')
+  .toString()
+  .split(process.platform === 'linux' ? '\n' : '\r\n');
+
+function solution(input) {
+  let stack = [];
+  let map = {
+    '(': ')',
+    '[': ']',
+  };
+
+  for (let e of input) {
+    if (e === '(' || e === '[') {
+      stack.push(e);
+    } else if (e === ')' || e === ']') {
+      let last = stack.pop();
+      if (map[last] !== e) {
+        return 'no';
+      }
+    }
+  }
+
+  return stack.length === 0 ? 'yes' : 'no';
+}
+
+let answer = [];
+for (let i = 0; i < input.length - 2; i++) {
+  answer.push(solution(input[i]));
+}
+
+console.log(answer.join('\n'));
