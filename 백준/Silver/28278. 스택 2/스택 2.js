879215f33f529339acc283f72cbfb022b38c4390
yangharry <120225915+yangharry@users.noreply.github.com>
2023-10-29 14:39:42 +0900

    [Silver IV] Title: 스택 2, Time: 1216 ms, Memory: 175140 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.js
@@ -0,0 +1,38 @@
+const [n, ...input] = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'ex0.txt')
+  .toString()
+  .split(process.platform === 'linux' ? '\n' : '\r\n');
+
+let stack = [];
+let answer = [];
+for (let i = 0; i < n; i++) {
+  const [a, b] = input[i].split(' ').map((e) => {
+    return (e = +e);
+  });
+
+  if (a == 1) {
+    stack.push(b);
+  } else if (a == 2) {
+    if (stack[0]) {
+      answer.push(stack.pop());
+    } else {
+      answer.push(-1);
+    }
+  } else if (a == 3) {
+    answer.push(stack.length);
+  } else if (a == 4) {
+    if (stack[0]) {
+      answer.push(0);
+    } else {
+      answer.push(1);
+    }
+  } else if (a == 5) {
+    if (stack[0]) {
+      answer.push(stack[stack.length - 1]);
+    } else {
+      answer.push(-1);
+    }
+  }
+}
+
+console.log(answer.join('\n'));
