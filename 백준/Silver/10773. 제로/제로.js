627db7e09ffe3b125fe75852ac73a2e158c9081e
yangharry <120225915+yangharry@users.noreply.github.com>
2023-10-29 14:52:29 +0900

    [Silver IV] Title: 제로, Time: 216 ms, Memory: 19968 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.js
+++ 백준/Silver/10773. 제로/제로.js
@@ -1,25 +1,20 @@
 const [n, ...input] = require('fs')
   .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'ex0.txt')
   .toString()
-  .split(process.platform === 'linux' ? '\n' : '\r\n')
-  .map((e) => (e = +e));
+  .split(process.platform === 'linux' ? '\n' : '\r\n');
 
 let answer = [];
 for (let i = 0; i < n; i++) {
-  let a = input[i];
-  if (a == '0') {
+  let a = Number(input[i]);
+  if (a == 0) {
     answer.pop();
   } else {
     answer.push(a);
   }
 }
 
-if (answer[0]) {
-  console.log(
-    answer.reduce((acc, cur) => {
-      return (acc = acc + cur);
-    })
-  );
-} else {
-  console.log(0);
-}
+console.log(
+  answer.reduce((acc, cur) => {
+    return acc + cur;
+  }, 0)
+);
