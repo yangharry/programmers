b1cea47adba00dda6a2b60af5793274393b391d7
yangharry <120225915+yangharry@users.noreply.github.com>
2023-10-30 15:44:50 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.46 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,16 +1,12 @@
 function solution(survey, choices) {
-    const MBTI = {};
-    const types = ["RT","CF","JM","AN"];
+    const data = { R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0 }
 
-    types.forEach((type) =>
-        type.split('').forEach((char) => MBTI[char] = 0)
-    )
+    for (let i = 0; i < survey.length; i++) {
+        const score = choices[i] - 4
+        let type = survey[i].split('')[score < 0 ? 0 : 1] 
+        data[type] += Math.abs(score)
+    }
 
-    choices.forEach((choice, index) => {
-        const [disagree, agree] = survey[index];
-
-        MBTI[choice > 4 ? agree : disagree] += Math.abs(choice - 4);
-    });
-
-    return types.map(([a, b]) => MBTI[b] > MBTI[a] ? b : a).join("");
+    const { R, T, C, F, J, M, A, N } = data
+    return `${R >= T ? 'R' : 'T'}${C >= F ? 'C' : 'F'}${J >= M ? 'J' : 'M'}${A >= N ? 'A' : 'N'}`
 }
