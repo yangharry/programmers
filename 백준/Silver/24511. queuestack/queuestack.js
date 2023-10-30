3413464410caf8c7cb8f3d140d1d8903e0a15236
yangharry <120225915+yangharry@users.noreply.github.com>
2023-10-30 12:34:35 +0900

    [Silver III] Title: queuestack, Time: 328 ms, Memory: 52820 KB -BaekjoonHub

---
+++ 백준/Silver/24511. queuestack/queuestack.js
@@ -0,0 +1,35 @@
+let [N, A, B, M, C] = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'ex0.txt')
+  .toString()
+  .split(process.platform === 'linux' ? '\n' : '\r\n');
+
+A = A.split(' ');
+B = B.split(' ');
+C = C.split(' ');
+
+let A0 = A.join('').replaceAll('0', '').length;
+let D = [];
+let j = N - 1;
+let k = 0;
+while (D.length < M) {
+  if (A[j] == '0') {
+    D.push(B[j]);
+  }
+  j--;
+  if (j < 0) {
+    D.push(C[k]);
+    k++;
+  }
+}
+
+let answer = [];
+
+for (let i = 0; i < M; i++) {
+  if (N - A0 == 0) {
+    answer.push(C[i]);
+  } else {
+    answer.push(D[i]);
+  }
+}
+
+console.log(answer.join(' '));
