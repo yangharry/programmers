c3912e52c2ffc9548ad914304eaefa9ee8870959
yangharry <120225915+yangharry@users.noreply.github.com>
2023-11-10 15:39:10 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 572 ms, Memory: 96048 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.js
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.js
@@ -1,4 +1,4 @@
-let [n, ...connections] = require('fs')
+let [n, ...input] = require('fs')
   .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'ex0.txt')
   .toString()
   .split(process.platform === 'linux' ? '\n' : '\r\n');
@@ -6,10 +6,9 @@ let [n, ...connections] = require('fs')
 n = Number(n);
 
 const tree = {};
-const parent = Array(n + 1).fill(0); // 부모 노드를 저장할 배열
+const parent = Array(n + 1).fill(0); 
 
-// 트리 구성
-connections.forEach((connection) => {
+input.forEach((connection) => {
   const [a, b] = connection.split(' ').map(Number);
   if (!tree[a]) tree[a] = [];
   if (!tree[b]) tree[b] = [];
@@ -21,15 +20,14 @@ function dfs(node, parentOfNode) {
   parent[node] = parentOfNode;
   for (const child of tree[node]) {
     if (child !== parentOfNode) {
-      // 이미 방문한 노드(부모 노드)는 제외
+     
       dfs(child, node);
     }
   }
 }
 
-// 루트 노드부터 DFS 실행
+
 dfs(1, 0);
 
-// 결과 출력 (1번 노드는 루트이므로 제외)
 const result = parent.slice(2).join('\n');
 console.log(result);
