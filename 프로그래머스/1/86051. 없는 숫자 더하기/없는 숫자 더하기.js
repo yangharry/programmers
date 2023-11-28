121983ffdd181fa61b1f6bc451167e51673aff2c
yangharry <120225915+yangharry@users.noreply.github.com>
2023-11-28 16:59:50 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,9 +1,3 @@
 function solution(numbers) {
-    var answer = 0;
-    for(let i = 0; i < 10; i++){
-        if(numbers.indexOf(i) === -1){
-            answer += i
-        }
-    }
-    return answer;
+    return 45 - numbers.reduce((cur, acc) => cur + acc, 0);
 }
