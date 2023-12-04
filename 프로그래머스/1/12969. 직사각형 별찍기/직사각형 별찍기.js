6931e68ec1957a6199aeabd20d0b8c3f513d0415
yangharry <120225915+yangharry@users.noreply.github.com>
2023-12-04 18:52:29 +0900

    [level 1] Title: 직사각형 별찍기, Time: 42.01 ms, Memory: 32.1 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -2,11 +2,5 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
-    let answer = ''
-    for(let i = 0 ; i < a; i++){
-        answer += '*'
-    }
-    for(let i = 0 ; i < b; i ++){
-        console.log(answer)
-    }
+    console.log(Array(b).fill('*'.repeat(a)).join('\n'))
 });
