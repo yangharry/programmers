2181ef1332db16252fd6b577b07cff411f483912
yangharry <120225915+yangharry@users.noreply.github.com>
2023-12-14 15:47:21 +0900

    [level 1] Title: 실패율, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,23 +1,10 @@
 function solution(N, stages) {
-    var answer = [];
-    let map = new Map()
-    for(let i = 1 ; i <= N+1 ; i ++){
-        map.set(i,[0,0])
+    let answer = [];
+    for(let i = 1 ; i <= N ; i ++){
+        let denominator = stages.filter((e)=> e >= i).length;
+        let molecule = stages.filter((e)=> e === i).length;
+        answer.push([i,molecule/denominator])
     }
-    stages.map((e)=>{
-        map.set(e,[map.get(e)[0]+1,map.get(e)[1]])
-        for(let i = 1; i <= e ; i++){
-            map.set(i,[map.get(i)[0],map.get(i)[1]+1])
-        }
-    })
-    let result = []
-    for(let i = 1 ; i <=N; i ++){
-        let [a,b] = map.get(i)
-        result.push([i, a/b])
-    }
-    result.sort((a,b)=> b[1] - a[1])
-    result.map(([a,b])=>{
-        answer.push(a)
-    })
-    return answer
+    answer.sort((a,b)=> b[1] - a[1])
+    return answer.map((e)=> e[0])
 }
