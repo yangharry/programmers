2c5a222a7cdcf2f86f0f1463b805a850e97f3d2c
yangharry <120225915+yangharry@users.noreply.github.com>
2023-12-15 16:59:52 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.19 ms, Memory: 33.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -0,0 +1,26 @@
+function solution(id_list, report, k) {
+    var answer = [];
+    let 신고한사람 = {}
+    let 신고당한사람 = {} 
+    id_list.map((e)=>{
+        신고한사람[e] = new Set()
+        신고당한사람[e] = new Set()
+    })
+    
+    report.map((e)=>{
+        let [delator,defendant] = e.split(" ");
+        신고당한사람[defendant].add(delator)
+        신고한사람[delator].add(defendant)
+    })
+    
+    id_list.map((a,i)=>{
+        let result = 0;
+        [...신고한사람[a]].map((b)=>{
+            if(신고당한사람[b].size >= k){
+                result ++
+            }
+        })
+        answer.push(result)
+    })
+    return answer;
+}
