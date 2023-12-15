a389e0145f55f36dc14b9c2c713f6664aaeab109
yangharry <120225915+yangharry@users.noreply.github.com>
2023-12-15 17:42:50 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,8 +1,13 @@
 function solution(new_id) {
-    let answer = new_id.toLowerCase().replace(/[^-_.a-z0-9]/g,'').replace(/\.{2,}/g,'.').replace(/^\.|\.$/g,'');
-    if(!answer) answer = 'a'
-    answer = answer.substring(0,15).replace(/\.$/, '')
+    let answer = new_id
+    .toLowerCase()
+    .replace(/[^-_.a-z0-9]/g,'')
+    .replace(/\.{2,}/g,'.')
+    .replace(/^\.|\.$/g,'')
+    .replace(/^$/,'a') // if(!answer) answer = 'a'
+    .substring(0,15)
+    .replace(/\.$/, '')
     while(answer.length < 3) answer += answer[answer.length -1]
- 
+
     return answer;
 }
