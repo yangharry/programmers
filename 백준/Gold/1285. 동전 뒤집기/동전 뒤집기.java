dffc9e78081b4897e6e088caa495fc0d75571419
yangharry <120225915+yangharry@users.noreply.github.com>
2024-01-03 15:13:43 +0900

    [Gold I] Title: 동전 뒤집기, Time: 652 ms, Memory: 22804 KB -BaekjoonHub

--- 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.java
+++ 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.java
@@ -2,6 +2,11 @@ import java.io.*;
 import java.util.*;
 
 class Main {
+
+    private static int N;
+    private static boolean[][] coins;
+    private static int minFlips;
+
     public static void main(String[] args) throws IOException {
         String osName = System.getProperty("os.name").toLowerCase();
         boolean isLinux = osName.contains("linux");
@@ -9,9 +14,9 @@ class Main {
         BufferedReader br = new BufferedReader(
                 new InputStreamReader(isLinux ? System.in : new FileInputStream("ex0.txt")));
 
-        int N = Integer.parseInt(br.readLine());
-        boolean[][] coins = new boolean[N][N];
+        N = Integer.parseInt(br.readLine());
 
+        coins = new boolean[N][N];
         for (int i = 0; i < N; i++) {
             String line = br.readLine();
             for (int j = 0; j < N; j++) {
@@ -19,29 +24,38 @@ class Main {
             }
         }
 
-        int minFlips = Integer.MAX_VALUE;
+        minFlips = Integer.MAX_VALUE;
+        dfs(0);
+        System.out.println(minFlips);
+    }
 
-        for (int bit = 0; bit < (1 << N); bit++) {
-            int filps = 0;
-            for (int i = 0; i < N; i++) {
-                int count = 0;
-                for (int j = 0; j < N; j++) {
-                    boolean current = coins[i][j];
-
-                    if ((bit & (1 << j)) != 0) {
-                        current = !current;
-                    }
-
-                    if (current)
-                        count++;
-                }
-                filps += Math.min(count, N - count);
-            }
-
-            minFlips = Math.min(minFlips, filps);
+    private static void dfs(int column) {
+        if (column == N) {
+            calculateMinFlips();
+            return;
         }
 
-        System.out.println(minFlips);
-        br.close();
+        dfs(column + 1);
+
+        for (int i = 0; i < N; i++) {
+            coins[i][column] = !coins[i][column];
+        }
+        dfs(column + 1);
+        for (int i = 0; i < N; i++) {
+            coins[i][column] = !coins[i][column];
+        }
+    }
+
+    private static void calculateMinFlips() {
+        int totalFlips = 0;
+        for (int i = 0; i < N; i++) {
+            int count = 0;
+            for (int j = 0; j < N; j++) {
+                if (coins[i][j])
+                    count++;
+            }
+            totalFlips += Math.min(count, N - count);
+        }
+        minFlips = Math.min(minFlips, totalFlips);
     }
 }
