09f35cac4d1ef2f69b362926008c0477635b8703
yangharry <120225915+yangharry@users.noreply.github.com>
2024-05-26 22:33:15 +0900

    [level 2] Title: 올바른 괄호, Time: 33.53 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,15 +1,10 @@
 function solution(s){
-    let stack = [];
-    for(let char of s){
-        if(char === '('){
-            stack.push(char);
-        } else if(char === ')'){
-            if(stack.length === 0){
-                return false
-            }
-            stack.pop();
+    let cum = 0
+    for (let paren of s) {
+        cum += paren === '('? 1: -1
+        if(cum < 0) {
+            return false
         }
     }
-    
-    return stack.length === 0;
+    return cum === 0? true: false;
 }
